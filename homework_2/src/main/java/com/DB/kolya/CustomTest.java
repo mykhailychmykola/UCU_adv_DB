f0bb28c6adc378c9605864e65b44ade97dd44aec
nickolaymykhalych <mykhalychnickolay@gmail.com>
2017-01-05 20:53:13 +0200

    Update CustomTest.java

--- homework_2/src/main/java/com/DB/kolya/CustomTest.java
+++ homework_2/src/main/java/com/DB/kolya/CustomTest.java
@@ -10,23 +10,24 @@ import java.io.IOException;
 import java.util.List;
 
 public class CustomTest extends BaseTest{
-
+    
     @Override
     public void customTest() throws IOException {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hbase_pu");
         EntityManager em = emf.createEntityManager();
 
         Query findQuery = em.createQuery("select p from Physician p");
-        List<com.DB.kolya.Physician> allPhysicians = findQuery.getResultList();
+        List<com.kek5.Physician> allPhysicians = findQuery.getResultList();
         Assert.assertNotEquals(0, allPhysicians.size());
 
         Query findQuery2 = em.createQuery("select p from Patient p where p.firstName = \'Patient1\'");
-        List<com.DB.kolya.Patient> allJohns = findQuery2.getResultList();
-        Assert.assertEquals(1, allJohns.size());
+        List<com.kek5.Patient> patients1 = findQuery2.getResultList();
+        Assert.assertNotEquals(0, patients1.size());
 
         Query findQuery3 = em.createQuery("select mr from MedicalRecord mr where mr.type = \'type2\'");
-        List<com.DB.kolya.MedicalRecord> allRecords = findQuery3.getResultList();
-        Assert.assertEquals(, allRecords.size());
+        List<com.kek5.MedicalRecord> allRecords2 = findQuery3.getResultList();
+        Assert.assertNotEquals(0, allRecords2.size());
     }
-
 }
+
+    
